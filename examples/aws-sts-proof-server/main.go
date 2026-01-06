94c9a4cd1539065b107ac3ccd19633fbbc9fb1ce
udhos <udhos>
2026-01-06 02:26:25 -0300

    Send sts presigned request.

--- examples/aws-sts-proof-server/main.go
+++ examples/aws-sts-proof-server/main.go
@@ -3,21 +3,22 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/udhos/aws-sts-proof/awsstsproof"
 	"github.com/udhos/boilerplate/awsconfig"
 )
 
 type application struct {
 	awsConfig aws.Config
-	stsClient *sts.Client
+	//stsClient *sts.Client
 }
 
 func main() {
@@ -48,7 +49,7 @@ func main() {
 
 	app := &application{
 		awsConfig: awsCfg.AwsConfig,
-		stsClient: sts.NewFromConfig(awsCfg.AwsConfig),
+		//stsClient: sts.NewFromConfig(awsCfg.AwsConfig),
 	}
 
 	const root = "/"
@@ -103,7 +104,7 @@ func handlerHealth(w http.ResponseWriter, r *http.Request) {
 	response(w, r, http.StatusOK, "health ok")
 }
 
-func handlerToken(w http.ResponseWriter, r *http.Request, app *application) {
+func handlerToken(w http.ResponseWriter, r *http.Request, _ *application) {
 
 	reqBody, errBody := io.ReadAll(r.Body)
 	if errBody != nil {
@@ -124,18 +125,99 @@ func handlerToken(w http.ResponseWriter, r *http.Request, app *application) {
 		return
 	}
 
-	log.Printf("FIXME %v", app.stsClient)
+	//log.Printf("FIXME %v", app.stsClient)
 
-	//
-	// FIXME WRITEME Get presigned request received on param (method, url, header),
-	// send it to AWS, get UserID from aws response.
-	//
-
+	// Forward the presigned request to AWS using a plain HTTP client
 	log.Printf("%s %s %s - raw body: %v", r.RemoteAddr, r.Method, r.RequestURI, string(reqBody))
-
 	log.Printf("%s %s %s - body:%s body-to-string:%s", r.RemoteAddr, r.Method, r.RequestURI, string(toJSON(param)), string(param.Body))
 
-	response(w, r, http.StatusOK, "ok")
+	// validate presigned request looks like STS GetCallerIdentity
+	if param.Method == "" {
+		response(w, r, http.StatusBadRequest, "missing method in presigned request")
+		return
+	}
+	if param.URL == "" {
+		response(w, r, http.StatusBadRequest, "missing url in presigned request")
+		return
+	}
+
+	if param.Method != "GET" {
+		response(w, r, http.StatusBadRequest, "presigned request must be GET")
+		return
+	}
+
+	u, errParse := url.Parse(param.URL)
+	if errParse != nil {
+		response(w, r, http.StatusBadRequest, "invalid url in presigned request")
+		return
+	}
+
+	// require Query Action=GetCallerIdentity
+	if u.Query().Get("Action") != "GetCallerIdentity" {
+		response(w, r, http.StatusBadRequest, "presigned request Action is not GetCallerIdentity")
+		return
+	}
+
+	// require signature: either Authorization header or X-Amz-Signature in query or headers
+	hasAuth := false
+	if _, ok := param.Headers["Authorization"]; ok {
+		hasAuth = true
+	}
+	if u.Query().Get("X-Amz-Signature") != "" {
+		hasAuth = true
+	}
+	if _, ok := param.Headers["X-Amz-Signature"]; ok {
+		hasAuth = true
+	}
+	if !hasAuth {
+		response(w, r, http.StatusBadRequest, "presigned request missing signature")
+		return
+	}
+
+	reqToAws, errReq := http.NewRequestWithContext(r.Context(), param.Method, param.URL, nil)
+	if errReq != nil {
+		response(w, r, http.StatusBadRequest, errReq.Error())
+		return
+	}
+	for k, vv := range param.Headers {
+		for _, v := range vv {
+			reqToAws.Header.Add(k, v)
+		}
+	}
+
+	respAws, errDo := http.DefaultClient.Do(reqToAws)
+	if errDo != nil {
+		response(w, r, http.StatusBadGateway, errDo.Error())
+		return
+	}
+	defer respAws.Body.Close()
+
+	respData, errRead := io.ReadAll(respAws.Body)
+	if errRead != nil {
+		response(w, r, http.StatusBadGateway, errRead.Error())
+		return
+	}
+
+	//log.Printf("sts response: %s", string(respData))
+
+	// Parse STS GetCallerIdentity XML response
+	var stsResp struct {
+		Result struct {
+			UserID  string `xml:"UserId"`
+			Account string `xml:"Account"`
+			Arn     string `xml:"Arn"`
+		} `xml:"GetCallerIdentityResult"`
+	}
+	if err := xml.Unmarshal(respData, &stsResp); err != nil {
+		log.Printf("xml unmarshal error: %v", err)
+		// return raw AWS body for debugging
+		response(w, r, http.StatusOK, string(respData))
+		return
+	}
+
+	iamARN := stsResp.Result.Arn
+	log.Printf("STS gerCallerIdentity ARN: %s", iamARN)
+	response(w, r, http.StatusOK, fmt.Sprintf("getCallerIdentity.ARN=%s", iamARN))
 }
 
 func toJSON(v any) []byte {
