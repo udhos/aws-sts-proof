46503c1f87ae61c2387ebcbb7079592b7d820190
udhos <udhos>
2026-01-06 01:55:44 -0300

    Sts client.

--- examples/aws-sts-proof-client/main.go
+++ examples/aws-sts-proof-client/main.go
@@ -9,10 +9,37 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/udhos/aws-sts-proof/awsstsproof"
+	"github.com/udhos/boilerplate/awsconfig"
 )
 
 func main() {
+
+	//
+	// aws config
+	//
+
+	options := awsconfig.Options{}
+	awsCfg, errCfg := awsconfig.AwsConfig(options)
+	if errCfg != nil {
+		log.Fatalf("could not get aws config: %v", errCfg)
+	}
+
+	log.Printf("STS account ID: %s\n", awsCfg.StsAccountID)
+	log.Printf("STS ARN: %s\n", awsCfg.StsArn)
+	log.Printf("STS UserId: %s\n", awsCfg.StsUserID)
+
+	//
+	// sts client
+	//
+
+	clientSts := sts.NewFromConfig(awsCfg.AwsConfig)
+	log.Printf("FIXME TODO REMOVEME %v", clientSts)
+
+	//
+	// FIXME TODO WRITEME: create sts getGetCallerIdentify pre-signed request
+	//
 
 	headers := http.Header{}
 	reqBody := []byte("test-request-body")
